0dd38756fa21cf6849eaa7715dff7a779d33d17f
Oliver Hees <oliverhees@gmail.com>
2023-06-29 16:51:36 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -42,12 +42,15 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>Weather App</h1>
+      </header>
       <main>
         <List
           activities={filterItems}
           isGoodWeather={goodWeather}
-          buttonHandler={buttonHandler} />
+          buttonHandler={buttonHandler}
+        />
         <Form handleActivities={handleActivities} />
       </main>
       <footer></footer>
